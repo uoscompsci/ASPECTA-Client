4f16bcc93223b8d744d82f98f185ec1fc202dcea
jp438 <jp438@localhost>
2014-02-21 18:02:14 +0000

    Restructured client to accept replies

--- src/testClient.py
+++ src/testClient.py
@@ -1,22 +1,42 @@
-import socket
-import sys
-ANY = '0.0.0.0'
-SENDERPORT=1501
-MCAST_ADDR = '224.168.2.9'
-MCAST_PORT = 1602
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-#The sender is bound on (0.0.0.0:1501)
-sock.bind((ANY,SENDERPORT))
-#Tell the kernel that we want to multicast and that the data is sent
-#to everyone (255 is the level of multicasting)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
-while 1:
-	#time.sleep(10)
-	#send the data 'hello, world' to the multicast addr: port
-	#Any subscribers to the multicast address will receive this data
-	message = raw_input("What message do you want to send?\n")
-	sock.sendto(message, (MCAST_ADDR,MCAST_PORT) )
-	if(message=='quit'):
-		print '\033[1;31mShutting down client\033[1;m'
-		sock.close()
-		sys.exit(0)
+# telnet program example
+import socket, select, string, sys
+
+host = 'localhost'
+port = 5000
+ 
+# main function
+if __name__ == "__main__":
+     
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+     
+    # connect to remote host
+    try :
+        s.connect((host, port))
+    except :
+        print 'Unable to connect'
+        sys.exit()
+     
+    print 'Connected to remote host. Start sending messages'
+     
+    while 1:
+        socket_list = [sys.stdin, s]
+         
+        # Get the list sockets which are readable
+        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+         
+        for sock in read_sockets:
+            # incoming message from remote server
+            if sock == s:
+                data = sock.recv(4096)
+                if not data :
+                    print '\nDisconnected from chat server'
+                    sys.exit()
+                else :
+                    # print data
+                    print data
+             
+            # user entered a message
+            else :
+                msg = sys.stdin.readline()
+                s.send(msg)
