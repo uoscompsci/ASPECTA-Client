03d1e6910f6703a85f59149ee4a2b9f58875eccd
jp438 <jp438@localhost>
2014-11-17 16:39:10 +0000

    The hello world code has now been uploaded.

---
+++ src/helloWorld.py
@@ -0,0 +1,9 @@
+from messageSender import *
+
+sender = messageSender()
+sender.login("username")
+sender.setapp("helloworld")
+height = sender.getSurfacePixelHeight(1)
+width = sender.getSurfacePixelWidth(1)
+win = sender.newWindow(1, 0, height, width, height, "HW")
+sender.newText(win, "Hello World", height/2-150, width/2-25, 60, "Free Sans", (1,1,1,1))
