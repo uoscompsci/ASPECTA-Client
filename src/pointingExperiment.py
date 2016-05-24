97801a3c3510552c3f939aaa6cd652eadbcf47b7
Julian Petford <julianpetford@gmail.com>
2016-05-24 15:59:55 +0100

    Code will now write to the path file with the correct file name when resuming.

--- src/pointingExperiment.py
+++ src/pointingExperiment.py
@@ -17,6 +17,7 @@ import csv
 from random import randint
 import datetime
 import os.path
+import os, glob
 
 
 class MyDialog(tkSimpleDialog.Dialog):
@@ -1086,7 +1087,13 @@ class client:
                     pass
             orderIndex = i+1
             writingPermissions = 'a'
-            print("Results file already exists. Starting with " + orderIndex)
+            for filename in glob.glob("results/" + str(self.USERNO) + "_trace_*"):
+                file = filename.split("/")[1]
+                file = file.split("_")
+                condition1 = file[2]
+                condition2 = file[3]
+                self.incrementTrialNumForCond(condition1, condition2)
+            print "Results file already exists. Starting with " + str(orderIndex)
 
         if newFile:
             with open("results/" + str(self.USERNO) + "_trialDetails.csv", 'w') as trialDetailsCSV:
@@ -1315,6 +1322,7 @@ class client:
                                 str(recordedPath[index]["angularVelocity"]) + "," +
                                 str(recordedPath[index]["velocity"]) + "," +
                                 str(recordedPath[index]["time"]) + "\n")
+        print "Wrote path file \"results/" + str(self.USERNO) + "_trace_" + CONDITION1 + "_" + CONDITION2 + "_" + str(self.getTrialNumForCond(CONDITION1, CONDITION2)) + ".csv\""
 
     def incrementTrialNumForCond(self, condition1, condition2):
         self.conditionCounter[condition1 + "," + condition2] += 1
